b153adf1cba95ad2903fa72675f11753e16a4c77
sbaraujo2012 <sb.araujo@yahoo.com.br>
2024-05-08 14:34:45 -0300

    locação

---
+++ Locacao/LocacaoRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace BikeRental.Locacao;
+
+public class LocacaoRepository
+{
+    public List<Locacao> SelectRentalsByBike(int idMoto)
+    {
+        return new List<Locacao>();
+    }
+    public List<Locacao> SelectRentalsByDelivery(int idEntregador)
+    {
+        return new List<Locacao>();
+    }
+}
---
+++ Locacao/LocacaoService.cs
@@ -0,0 +1,15 @@
+﻿namespace BikeRental.Locacao;
+
+public class LocacaoService
+{
+    private LocacaoRepository _repository = new LocacaoRepository();
+
+    public List<Locacao> MotoHistoricoLocacao(int idMoto)
+    {
+        return _repository.SelectRentalsByBike(idMoto);
+    }
+    public List<Locacao> EntregadorHistoricoLocacao(int idEntregador)
+    {
+        return _repository.SelectRentalsByDelivery(idEntregador);
+    }
+}
