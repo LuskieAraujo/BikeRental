1d7bf5b0e60123963ae5b0604be42d027ae9341f
Lucas Araújo <araujo_depaula@outlook.com>
2024-05-15 14:49:53 -0300

    ano da moto de string para int

--- Moto/Moto.cs
+++ Moto/Moto.cs
@@ -3,7 +3,7 @@ namespace BikeRental.Moto;
 public class Moto
 {
 	public int Id { get; set; }
-	public string Ano { get; set; }
+	public int Ano { get; set; }
 	public string Modelo { get; set; }
 	public string Placa { get; set; }
 	public bool Alugada { get; set; }
